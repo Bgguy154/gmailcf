52144f25182e0078c0274a75694485171fda8109
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-08-15 12:52:13 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     const navigate = useNavigate();
     const logoutHandler = async () => {
         try {
-            const res = await axios.get('http://localhost:8000/api/v1/user/logout',{withCredentials:true});
+            const res = await axios.get('https://gmailcb.vercel.app/api/v1/user/logout',{withCredentials:true});
             console.log(res);
             toast.success(res.data.message);
             dispatch(setAuthUser(null));
