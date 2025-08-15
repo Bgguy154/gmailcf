7389ede15c94fa528a3ef298ebc90dcd66f9d0d4
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-08-15 12:51:25 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
         e.preventDefault();
         console.log(input);
         try {
-            const res = await axios.post("http://localhost:8000/api/v1/user/login", input, {
+            const res = await axios.post("https://gmailcb.vercel.app/api/v1/user/login", input, {
                 headers:{
                     'Content-Type':"application/json"
                 },
